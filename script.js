636af50a093d46592f1065e8c40dabb8c684c7ca
PrinceCharlesTurkson <Elizabethben793@gmail.com>
2023-06-27 12:22:28 +0000

    commit

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const card2 = document.querySelector('.card2');
 const card3 = document.querySelector('.card3')
 const sideHeading = document.getElementById('sideHeading');
 const sideParagraph = document.getElementById('intoDetails');
-
+const additionalDetails = document.getElementById('additionalDetails');
 
 
 
@@ -13,7 +13,8 @@ card.addEventListener('click', function() {
   card2.classList.remove('Awwward');
   card3.classList.remove('Awwward');
   sideHeading.textContent = 'Quae a perfectionist.';
-  sideParagraph.textContent = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem quae molestiae nihil soluta ut cupiditate error itaque laudantium consequuntur sapiente mollitia explicabo, minima veniam asperiores architecto? Quidem atque consequatur natus eius inventore nisi. Vitae provident assumenda praesentium\n\nLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem quae molestiae nihil soluta ut cupiditate error itaque laudantium consequuntur sapiente mollitia explicabo, minima veniam asperiores architecto? Quidem atque consequatur natus eius inventore nisi. Vitae provident assumenda praesentium`;
+  sideParagraph.textContent = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem quae molestiae nihil soluta ut cupiditate error itaque laudantium consequuntur sapiente mollitia explicabo, minima veniam asperiores architecto? Quidem atque consequatur natus eius inventore nisi. Vitae provident assumenda praesentium`;
+  additionalDetails.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem quae molestiae nihil soluta ut cupiditate error itaque laudantium consequuntur sapiente mollitia explicabo, minima veniam asperiores architecto? Quidem atque consequatur natus eius inventore nisi. Vitae provident assumenda praesentium'
   
   console.log('changed');
 });
@@ -23,8 +24,8 @@ card2.addEventListener('click', function() {
   card2.classList.add('Awwward');
   card3.classList.remove('Awwward');
   sideHeading.textContent = 'laborantem describitur eius';
-  sideParagraph.textContent = `Checkmate... So you two dig up, dig up dinosaurs? They're using our own satellites against us. And the clock is ticking. Checkmate... God creates dinosaurs. God destroys dinosaurs. God creates Man. Man destroys God. Man creates Dinosaurs. You're a very talented young man, with your own clever thoughts and ideas. Do you need a manager? Remind me to thank John for a lovely weekend. God help us, we're in the hands of engineers. God help us, we're in the hands of engineers. They're using our own satellites against us. And the clock is ticking. My dad once told me, laugh and the world laughs with you, Cry, and I'll give you something to cry about you little bastard!`;
-  
+  sideParagraph.textContent = `Checkmate... So you two dig up, dig up dinosaurs? They're using our own satellites against us. And the clock is ticking. Checkmate... God creates dinosaurs. God destroys dinosaurs. God creates Man. Man destroys God. Man creates Dinosaurs. You're a very talented young man, with your own clever thoughts and ideas. Do you need a manager? Remind me to thank John for a lovely weekend.\n\n`;
+  additionalDetails.textContent = ' God help us, we are in the hands of engineers. God help us, we are in the hands of engineers. Theyre using our own satellites against us. And the clock is ticking. My dad once told me, laugh and the world laughs with you, Cry, and I will give you something to cry about you little bastard!'
   
 });
 
